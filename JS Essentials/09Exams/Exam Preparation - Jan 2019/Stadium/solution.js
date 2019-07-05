85e45957016db077a240936c188162e87610ef91
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-07-05 16:07:32 +0300

    Better solutions for some JS Essentials Exam tasks

---
+++ JS Essentials/09Exams/Exam Preparation - Jan 2019/Stadium/solution.js
@@ -0,0 +1,54 @@
+function solve() {
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    let output = document.getElementById('output');
+    let obj = {
+        'Levski':
+        {
+            'A': 10,
+            'B': 7,
+            'C': 5
+        },
+        'Litex':
+        {
+            'A': 10,
+            'B': 7,
+            'C': 5
+        },
+        'VIP':
+        {
+            'A': 25,
+            'B': 15,
+            'C': 10
+        },
+        '__summary__': {
+            'fans': 0,
+            'totalProfit': 0
+        }
+    }
+    for (let index = 0; index < buttons.length - 1; index++) {
+        let button = buttons[index];
+        button.addEventListener('click', clickEvent);
+    };
+
+    function clickEvent(e) {
+        let seat = e.target;
+        let seatNum = seat.textContent;
+        let zone = seat.parentNode.parentNode.parentNode.parentNode.parentNode.className;
+        let sector = String.fromCharCode(65 + e.target.parentNode.cellIndex);
+        if (seat.style.backgroundColor == '') {
+            seat.style.backgroundColor = 'rgb(255,0,0)';
+            let price = obj[zone][sector];
+            obj.__summary__.fans += 1;
+            obj.__summary__.totalProfit += price;
+            output.textContent += ` Seat ${seatNum} in zone ${zone} sector ${sector} was taken.\n`;
+        } else {
+            output.textContent += ` Seat ${seatNum} in zone ${zone} sector ${sector} is unavailable.\n`;
+        }
+    }
+    let summaryButton = buttons[buttons.length - 1];
+    let summary = document.getElementById('summary');
+    let spanElementResult = summary.getElementsByTagName('span')[0];
+    summaryButton.addEventListener('click', () => {
+        spanElementResult.textContent = `${obj.__summary__.totalProfit} leva, ${obj.__summary__.fans} fans.`;
+    });
+}
---
+++ JS Essentials/09Exams/Exam Preparation I - May 2019/01Courses Prices/01CoursePrices.js
@@ -0,0 +1,27 @@
+function solve(fundamentals, advanced, applications, type) {
+    let total = 0;
+    if (fundamentals) {
+        total += 170;
+    }
+    if (advanced) {
+        total += 180;
+    }
+    if (applications) {
+        total += 190;
+    }
+    if (fundamentals
+        && advanced) {
+        total -= (0.1 * 180);
+    }
+    if (fundamentals
+        && advanced
+        && applications) {
+        total *= 0.94;
+    }
+    if (type == 'online') {
+        total *= 0.94;
+    }
+    console.log(Math.round(total));
+}
+solve(true, true, false, 'onsite');
+
