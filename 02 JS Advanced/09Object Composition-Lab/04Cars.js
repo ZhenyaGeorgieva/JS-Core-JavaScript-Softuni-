1a007b23df1a8a7902cf02420e10ed44d9619a64
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-07-22 11:40:05 +0300

    Folder arrangement Object Composition lab

--- 02 JS Advanced/09Object Composition-Lab/04Cars.js
+++ 02 JS Advanced/09Object Composition-Lab/04Cars.js
@@ -2,26 +2,26 @@ function processCommands(params) {
     let cars = {}
     let commandProcessor = (() => {
         const create = ([car, inherit, parentCar]) => {
-            let newCar = inherit ? Object.create(cars[parentCar]) : {}
-            cars[car] = newCar
+            let newCar = inherit ? Object.create(cars[parentCar]) : {};
+            cars[car] = newCar;
         }
 
-        const set = ([car, key, value]) => (cars[car][key] = value)
+        const set = ([car, key, value]) => (cars[car][key] = value);
         const print = ([car]) => {
 
-            let carProperties = []
+            let carProperties = [];
 
             for (let key in cars[car]) {
-                carProperties.push(`${key}:${cars[car][key]}`)
+                carProperties.push(`${key}:${cars[car][key]}`);
             }
-            console.log(carProperties.join(', '))
+            console.log(carProperties.join(', '));
         }
-        return { create, set, print }
+        return { create, set, print };
     })()
 
     params
         .map(p => p.split(' '))
-        .forEach(([command, ...args]) => commandProcessor[command](args))
+        .forEach(([command, ...args]) => commandProcessor[command](args));
 }
 
 processCommands([
